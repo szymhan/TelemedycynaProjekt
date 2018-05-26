d55519d3e4a01f7d6339fa6567ce05c3446431c4
Szymon Hanzel <szyhan@gmail.com>
2018-05-26 12:31:31 +0200

    początek

---
+++ app/src/main/java/pl/domatslaski/telemedycynaprojekt/DeviceScanActivity.java
@@ -0,0 +1,13 @@
+package pl.domatslaski.telemedycynaprojekt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DeviceScanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_scan);
+    }
+}
