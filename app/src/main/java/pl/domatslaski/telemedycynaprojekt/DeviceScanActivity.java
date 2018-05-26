bf48f2dd69b68aba6a60bc335d24a2581776c232
Szymon Hanzel <szyhan@gmail.com>
2018-05-26 16:48:43 +0200

    początek

--- app/src/main/java/pl/domatslaski/telemedycynaprojekt/DeviceScanActivity.java
+++ app/src/main/java/pl/domatslaski/telemedycynaprojekt/DeviceScanActivity.java
@@ -1,6 +1,10 @@
 package pl.domatslaski.telemedycynaprojekt;
 
 import android.app.ListActivity;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanResult;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.Manifest;
 import android.app.Activity;
@@ -20,8 +24,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,22 +37,106 @@ public class DeviceScanActivity extends ListActivity {
 
     private LeDeviceListAdapter mLeDeviceListAdapter;
     private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothLeScanner mBluetoothLeScanner;
     private boolean mScanning;
     private Handler mHandler;
     public static int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION =1;
     private static final int REQUEST_ENABLE_BT = 1;
     // Stops scanning after 10 seconds.
     private static final long SCAN_PERIOD = 10000;
+    ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_scan);
+        mProgressBar = findViewById(R.id.progressBar);
+        mHandler = new Handler();
+
+        int permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+
+
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        } else {
+            if (areLocationServicesEnabled(this)) {
+                mHandler = new Handler();
+
+                if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+                    Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+
+                final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+                mBluetoothAdapter = bluetoothManager.getAdapter();
+                mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
+
+                scanLeDevice(true);
+
+
+            }
+        }
+        // Checks if Bluetooth is supported on the device.
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, R.string.error_bluetooth_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+
+        }
     }
 
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        scanLeDevice(false);
+        mLeDeviceListAdapter.clear();
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
 
+        // Ensures Bluetooth is enabled on the device.  If Bluetooth is not currently enabled,
+        // fire an intent to display a dialog asking the user to grant permission to enable it.
+        if (!mBluetoothAdapter.isEnabled()) {
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+        }
+
+        // Initializes list view adapter.
+        mLeDeviceListAdapter = new LeDeviceListAdapter();
+        setListAdapter(mLeDeviceListAdapter);
+        scanLeDevice(true);
+    }
+
+    private void scanLeDevice(final boolean enable) {
+        mProgressBar = findViewById(R.id.progressBar);
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                    mProgressBar.setVisibility(View.GONE);
+
+                }
+            }, SCAN_PERIOD);
+
+            mScanning = true;
+            mBluetoothAdapter.startLeScan(mLeScanCallback);
+            mProgressBar.setVisibility(View.VISIBLE);
+           // mBluetoothLeScanner.startScan(mLeScanCallback);
+        } else {
+            mScanning = false;
+          mBluetoothAdapter.stopLeScan(mLeScanCallback);
+            mProgressBar.setVisibility(View.GONE);
+        }
+        //invalidateOptionsMenu();
+    }
 
 
 
@@ -79,7 +169,7 @@ public class DeviceScanActivity extends ListActivity {
         }
 
         public void addDevice(BluetoothDevice device) {
-            if(!mLeDevices.contains(device)) {
+            if (!mLeDevices.contains(device)) {
                 mLeDevices.add(device);
             }
         }
@@ -126,14 +216,33 @@ public class DeviceScanActivity extends ListActivity {
             if (deviceName != null && deviceName.length() > 0)
                 viewHolder.deviceName.setText(deviceName);
             else
-                viewHolder.deviceName.setText(R.string.unknown_device);
+                viewHolder.deviceName.setText(R.string.unknownDevice);
             viewHolder.deviceAddress.setText(device.getAddress());
 
             return view;
         }
     }
+
     static class ViewHolder {
         TextView deviceName;
         TextView deviceAddress;
     }
+
+    public boolean areLocationServicesEnabled (Context context){
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        try {
+            return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                    locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
 }
+
+
+
+
+
